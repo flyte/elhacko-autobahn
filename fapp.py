69153f5e7a6407a72aca48aad8fd38738a5483b2
Michael Klich <michal@michalklich.com>
2015-11-14 23:07:29 +0000

    Add req file, add logging

--- fapp.py
+++ fapp.py
@@ -1,19 +1,26 @@
 import json
+import traceback
 from os import environ
 
+import requests
 from twisted.internet.defer import inlineCallbacks
 
 from autobahn.twisted.wamp import ApplicationSession, ApplicationRunner
+
+LOGGLY_URL = 'http://logs-01.loggly.com/inputs/3e7211c1-708e-4ec2-9144-2d47a5d03a60/tag/http/'
 
 
 class Component(ApplicationSession):
 
     def on_media_message(self, media_meta):
-        # TODO: loggly?
         # check uuid and send rest to subscriber
         try:
             media_meta = json.loads(media_meta)
+            requests.post(LOGGLY_URL, data=media_meta)
         except TypeError:
+            requests.post(LOGGLY_URL, data='Unpacking media_meta failed')
+            err = traceback.format_exc()
+            requests.post(LOGGLY_URL, data=err)
             pass
         self.publish(u'ws.media.{uuid}'.format(uuid=media_meta.get('uuid')),
                      json.dumps(media_meta))
